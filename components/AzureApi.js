9c3085b274b07d8216357eb14271566d34c11cbc
Administrator <oscar.brooks@dev9.com>
2019-01-09 22:09:00 +0000

    Add new file azure api

---
+++ components/AzureApi.js
@@ -0,0 +1,39 @@
+const msRestAzure = require('ms-rest-azure');
+const ApiManagementClient = require("azure-arm-apimanagement");
+
+module.exports = {
+    deleteApi,
+    createOrUpdate,
+}
+
+function deleteApi(resourceGroupName, serviceName, apiId, tenantId, subscriptionId, clientId, clientSecret) {
+    msRestAzure.loginWithServicePrincipalSecret(clientId, clientSecret, tenantId, function(err, credentials) {
+        if (err) return console.log(err);
+        const client = new ApiManagementClient(credentials, subscriptionId);
+        
+        client.api.deleteMethod(resourceGroupName, serviceName, apiId, "*", function(err, result) {
+          if (err) return console.log(err);
+          return console.log(result);
+        });
+      });
+}
+
+function createOrUpdate(resourceGroupName, serviceName, apiId, tenantId, subscriptionId, swaggerString, basepath, clientId, clientSecret) {
+    if (swaggerUrl != null && swaggerUrl != '' && swaggerUrl != 'undefined') {
+       
+    var parameters = {
+          "contentFormat": "swagger-json",
+          "contentValue": swaggerString,
+          "path": basepath
+        }
+    }
+    msRestAzure.loginWithServicePrincipalSecret(clientId, clientSecret, tenantId, function(err, credentials) {
+        if (err) return console.log(err);
+        const client = new ApiManagementClient(credentials, subscriptionId);
+        
+        client.api.createOrUpdate(resourceGroupName, serviceName, apiId, parameters, function(err, result) {
+          if (err) return console.log(err);
+          return console.log(result);
+        });
+      });
+    }
