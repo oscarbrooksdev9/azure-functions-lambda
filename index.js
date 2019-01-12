a60b80e87705dce1c276215edfe6569b2cd6d2d9
Administrator <oscar.brooks@dev9.com>
2019-01-12 02:23:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,11 @@ module.exports.handler = async (event, context) => {
   const config = configModule.getConfig(event, context);
   const errorHandler = errorHandlerModule();
   logger.init(event, context);
-
+  var result;
   try {
 
-    
     var data = event.data;
-    var result;
+    
     if(event.action == "delete"){
         logger.info('attempting to delete an API Gateway' + event.resourceGroupName);
         azureApi.deleteApi(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.clientId, data.clientSecret);
@@ -31,36 +30,10 @@ module.exports.handler = async (event, context) => {
     else if(event.action == "create"){
         logger.info('the value of data is: ' + event.action);
         logger.info('swagger data is: ' + data.swagger);
-        var parameters = {
-          "contentFormat": "swagger-json",
-          "contentValue": data.swagger,
-          "path": data.basepath
-        };
-        logger.info('parameters is: ' + parameters);
-        result = azureApi.createOrUpdate(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.swagger, data.basepath, data.clientId, data.clientSecret);
-        
-        // .catch((error) => {
-        //     throw new Error("error: " + JSON.stringify(error));
-        // });
+ 
+        result = await azureApi.createOrUpdate(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.swagger, data.basepath, data.clientId, data.clientSecret).promise();
+
     }
-    
-    //Following is a code snippet to fetch values from config file:
-    const myVal = config.configKey;
-
-    //Following code snippet describes how to log messages within your code:
-    /*
-    logger.error('Runtime errors or unexpected conditions.');
-    logger.warn('Runtime situations that are undesirable or unexpected, but not necessarily "wrong".');
-    logger.info('Interesting runtime events (Eg. connection established, data fetched etc.)');
-    logger.verbose('Generally speaking, most lines logged by your application should be written as verbose.');
-    logger.debug('Detailed information on the flow through the system.');
-    */
-
-    const sampleResponse = {
-      "foo": "this is a test of package json",
-      "bar": "bar-value",
-      "configKeys": myVal
-    };
 
     return responseObj(result, event);
 
@@ -74,6 +47,6 @@ module.exports.handler = async (event, context) => {
     //Sample Error response for Input Validation Error
     //cb(JSON.stringify(errorHandler.throwInputValidationError("Sample message")));
   }
-
+return result;
 
 };
