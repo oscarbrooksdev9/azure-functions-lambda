c5e1748863e2f10ab449671c78ca736c7283d7b8
Administrator <oscar.brooks@dev9.com>
2019-01-16 07:06:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ module.exports.handler = async (event, context) => {
 //   logger.init(event, context);
   var result;
   var data = event.data;
-  
   try {
     if(event.action == "delete"){
         result = await azureApi.deleteApi(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.clientId, data.clientSecret);
