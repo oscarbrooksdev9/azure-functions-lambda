95d0316aead0117c7094c373fe1107623bd00f19
Administrator <oscar.brooks@dev9.com>
2019-01-29 17:39:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,37 +8,33 @@ const configModule = require("./components/config.js");
 const logger = require("./components/logger.js");
 const responseObj = require("./components/response.js");
 const errorHandlerModule = require("./components/error-handler.js");
-
 const azureApi = require("./components/azureApi.js");
+const CommandMapping = require("./components/CommandMapping.js"); 
 
 module.exports.handler = async (event, context) => {
 
   //Initializations
   const config = configModule.getConfig(event, context);
   const errorHandler = errorHandlerModule();
-//   logger.init(event, context);
+  logger.init(event, context);
   var result;
   var data = event.data;
+  
+  var commandMapping = new CommandMapping();
+
   try {
+    result = await commandMapping.process(data);
+
     if(event.action == "delete"){
         result = await azureApi.deleteApi(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.clientId, data.clientSecret);
     }
     else if(event.action == "create"){
         result = await azureApi.createOrUpdate(data.resourceGroupName, data.serviceName, data.apiId, data.tenantId, data.subscriptionId, data.swagger, data.basepath, data.clientId, data.clientSecret);
     }
-    // return result;
   } 
   
   catch (error) {
       throw(error);
-    // return JSON.stringify(error.message);
-    result = { 
-         'status':400, 
-         'error':{
-             'error_message' : 'your message',
-             'details' : JSON.stringify(error)
-          }
-     }
   }
   return result;
 }
